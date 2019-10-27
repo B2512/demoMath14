279e10f3f5822893583e0a259bde8af8945212d6
Martw <91098897@qq.com>
2019-10-27 22:51:47 +0800

    Add yeqingnan

---
+++ src/b2512/yeqingnan/Main.java
@@ -0,0 +1,57 @@
+package b2512.yeqingnan;
+
+public class Main {
+    public static void main(String[] args) {
+        int height = 5;
+        int width = 5;
+        int mine = 3;
+        char[][] lattice = new char[height][width];
+        for (int i = 0; i < mine; i++) {
+            int random1 = (int) (Math.random() * height);
+            int random2 = (int) (Math.random() * width);
+            if (lattice[random1][random2] == '*') {
+                i--;
+            } else {
+                lattice[random1][random2] = '*';
+            }
+        }
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int result = 0;
+                if (lattice[i][j] == '*') {
+                    System.out.print(lattice[i][j]);
+                    continue;
+                } else {
+                    if (j != 0 && lattice[i][j - 1] == '*') {
+                        result++;
+                    }
+                    if (j != 0 && i != 0 && lattice[i - 1][j - 1] == '*') {
+                        result++;
+                    }
+                    if (i != 0 && lattice[i - 1][j] == '*') {
+                        result++;
+                    }
+                    if (i != 0 && j != lattice[0].length - 1 && lattice[i - 1][j + 1] == '*'){
+                        result++;
+                    }
+                    if (j != lattice[0].length - 1 && lattice[i][j + 1] == '*'){
+                        result++;
+                    }
+                    if (i != lattice.length - 1 && j != lattice[0].length - 1 && lattice[i + 1][j + 1] == '*'){
+                        result++;
+                    }
+                    if (i != lattice.length - 1 && lattice[i + 1][j] == '*'){
+                        result++;
+                    }
+                    if (i != lattice.length - 1 && j != 0 && lattice[i + 1][j - 1] == '*'){
+                        result++;
+                    }
+                }
+                lattice[i][j] = String.valueOf(result).charAt(0);
+                System.out.print(lattice[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
